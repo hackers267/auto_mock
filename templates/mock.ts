204d4e4161a12c62b022141c142d1e6f1b7935fa
silence_zhpf <silence_zhpf@aliyun.com>
2021-03-30 21:47:35 +0800

    feat(template): add the mock file
    add the mock file

---
+++ templates/mock.ts
@@ -0,0 +1,21 @@
+export default {
+  "Post /api/login": {
+    success: true,
+    data: {
+      total: 12,
+      list: [],
+    },
+  },
+};
+
+export const mock = `
+export default {
+    "Post /api/login": {
+        success:true,
+        data:{
+            total: 12,
+            list:[]
+        }
+    }
+}
+`;
