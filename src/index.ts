3822f7919d989984f47122fdc7bedf55e156868f
silence <silence_zhpf@aliyun.com>
2021-03-31 08:58:31 +0800

    feat: 添加 replaceRequestUrl

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,26 @@ import { parse } from "@babel/parser";
 import traverse from "@babel/traverse";
 import * as t from "@babel/types";
 import { mock } from "../templates/mock";
+import generate from "@babel/generator";
 
 const ast = parse(mock, { sourceType: "module", plugins: ["typescript"] });
 traverse(ast, {
   ObjectExpression(path) {
-    if (t.isObjectExpression(path.node)) {
-      console.log(path.node.properties);
+    replaceRequestUrl(`Post /api/logout`);
+
+    function replaceRequestUrl(url: string) {
+      if (t.isObjectExpression(path.node)) {
+        const properties = path.node.properties;
+        properties.forEach((property) => {
+          if (t.isObjectProperty(property)) {
+            const key = property.key;
+            if (t.isStringLiteral(key)) {
+              key.value = url;
+            }
+          }
+        });
+      }
     }
   },
 });
+generate(ast).code;
